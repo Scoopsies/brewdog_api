2e792c37405951525e627e61e049b117f15202bf
Scoopsies <mooninites103@gmail.com>
2024-04-16 20:30:06 -0500

    added logic for query information filter

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,44 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const punkapi = require('punkapi-db').sort((a, b) => a.id - b.id);
+const punkapiLib = require('punkapi-lib');
 const port = process.env.PORT || 8080;
 const app = (0, express_1.default)();
+const queryFilter = (beerData, query) => {
+    let filteredBeerData = beerData;
+    if (query.beer_name) {
+        const name = query.beer_name.toLowerCase();
+        filteredBeerData = filteredBeerData.filter(beer => {
+            return beer.name.toLowerCase().includes(name);
+        });
+    }
+    if (query.abv_gt) {
+        const abvLow = parseInt(query.abv_gt);
+        filteredBeerData = filteredBeerData.filter(beer => {
+            return beer.abv > abvLow;
+        });
+    }
+    if (query.abv_lt) {
+        const abvHigh = parseInt(query.abv_lt);
+        filteredBeerData = filteredBeerData.filter(beer => {
+            return beer.abv < abvHigh;
+        });
+    }
+    if (query.ibu_gt) {
+        const ibuLow = parseInt(query.ibu_gt);
+        filteredBeerData = filteredBeerData.filter(beer => {
+            return beer.ibu > ibuLow;
+        });
+    }
+    if (query.ibu_lt) {
+        const ibuHigh = parseInt(query.ibu_lt);
+        filteredBeerData = filteredBeerData.filter(beer => {
+            return beer.ibu < ibuHigh;
+        });
+    }
+    console.log('filtered beer data', filteredBeerData);
+    return filteredBeerData;
+};
 app.use((0, cors_1.default)({
     origin: ['https://diy-dog.onrender.com', 'http://localhost:5173'],
     methods: ["GET"]
@@ -17,8 +53,9 @@ app.get('/', (req, res) => {
     res.send("Hello from express + ts (not ts yet)");
 });
 app.get('/beers', (req, res) => {
+    console.log(req.query);
     console.log('someone accessed /beers');
-    res.json(punkapi);
+    res.json(queryFilter(punkapi, req.query));
 });
 app.get('/beer/:id', (req, res) => {
     console.log('someone accessed /beer/:id');
--- index.ts
+++ index.ts
@@ -1,10 +1,54 @@
 import express from 'express'; 
 import cors from 'cors';
 import BeerType from './beertype.types';
-const punkapi : BeerType[] = require('punkapi-db').sort((a : BeerType, b : BeerType) => a.id - b.id)
+const punkapi: BeerType[] = require('punkapi-db').sort((a : BeerType, b : BeerType) => a.id - b.id);
+const punkapiLib = require('punkapi-lib')
 const port = process.env.PORT || 8080;
 
+type Query = {
+    beer_name?: string,
+    abv_lt?: string,
+    abv_gt?: string,
+    ibu_lt?: string,
+    ibu_gt?: string
+}
 const app = express();
+
+const queryFilter = (beerData : BeerType[], query : Query)  => {
+    let filteredBeerData = beerData
+    if (query.beer_name) {
+        const name = query.beer_name.toLowerCase()
+        filteredBeerData = filteredBeerData.filter(beer => {
+           return beer.name.toLowerCase().includes(name)
+        }) 
+    }
+    if (query.abv_gt) {
+        const abvLow = parseInt(query.abv_gt)
+        filteredBeerData = filteredBeerData.filter(beer => {
+            return beer.abv > abvLow
+        })
+    }
+    if (query.abv_lt) {
+        const abvHigh = parseInt(query.abv_lt)
+        filteredBeerData = filteredBeerData.filter(beer => {
+            return beer.abv < abvHigh
+        })
+    }
+    if (query.ibu_gt) {
+        const ibuLow = parseInt(query.ibu_gt)
+        filteredBeerData = filteredBeerData.filter(beer => {
+            return beer.ibu > ibuLow
+        })
+    }
+    if (query.ibu_lt) {
+        const ibuHigh = parseInt(query.ibu_lt)
+        filteredBeerData = filteredBeerData.filter(beer => {
+            return beer.ibu < ibuHigh
+        })
+    }
+    console.log('filtered beer data',filteredBeerData)
+    return filteredBeerData
+}
 
 app.use(cors({
     origin: ['https://diy-dog.onrender.com', 'http://localhost:5173'],
@@ -17,8 +61,9 @@ app.get('/', (req, res) => {
 })
 
 app.get('/beers', (req, res) => {
+    console.log(req.query)
     console.log('someone accessed /beers')
-    res.json(punkapi)
+    res.json(queryFilter(punkapi, req.query))
 })
 
 app.get('/beer/:id', (req, res) => {
