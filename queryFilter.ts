8de4593ce73fe39a2d799fe285cf78754c08db9a
Scoopsies <mooninites103@gmail.com>
2024-04-17 13:04:44 -0500

    no logic changes, just readability changes

--- queryFilter.ts
+++ queryFilter.ts
@@ -1,50 +1,37 @@
-import BeerType from "./types/Beertype.types"
-import Query from "./types/Query.types"
+import BeerType from "./types/Beertype.types";
+import Query from "./types/Query.types";
 
-const filterByName = (beerData : BeerType[], nameQuery : string) => {
-    return beerData.filter(beer => beer.name.toLowerCase().includes(nameQuery.toLowerCase()))
-}
-const filterByAbvGreaterThan = (beerData : BeerType[], query: string ) => {
-    return beerData.filter(beer => beer.abv > parseFloat(query))
-}
-const filterByAbvLessThan = (beerData : BeerType[], query: string ) => {
-    return beerData.filter(beer => beer.abv < parseFloat(query))
-}
-const filterByIbuGreaterThan = (beerData : BeerType[], query: string ) => {
-    return beerData.filter(beer => beer.ibu > parseFloat(query))
-}
-const filterByIbuLessThan = (beerData : BeerType[], query: string ) => {
-    return beerData.filter(beer => beer.ibu < parseFloat(query))
-}
+const filterByName = (beerData: BeerType[], nameQuery: string) => {
+  return beerData.filter((beer) =>
+    beer.name.toLowerCase().includes(nameQuery.toLowerCase())
+  );
+};
 
-const queryFilter = (beerData : BeerType[], query : Query)  => {
-    const {beer_name, abv_gt, abv_lt, ibu_gt, ibu_lt} = query
+const filterByAbvGreaterThan = (beerData: BeerType[], query: string) => {
+  return beerData.filter((beer) => beer.abv > parseFloat(query));
+};
 
-    let filteredBeerData = beerData
+const filterByAbvLessThan = (beerData: BeerType[], query: string) => {
+  return beerData.filter((beer) => beer.abv < parseFloat(query));
+};
 
-    if (beer_name) {
-        filteredBeerData = filterByName(filteredBeerData, beer_name)
-    }
-    if (abv_gt) {
-        filteredBeerData = filterByAbvGreaterThan(filteredBeerData, abv_gt)
-    }
-    if (abv_lt) {
-        filteredBeerData = filterByAbvLessThan(filteredBeerData, abv_lt)
-    }
-    if (ibu_gt) {
-        const ibuLow = parseInt(ibu_gt)
-        filteredBeerData = filteredBeerData.filter(beer => {
-            return beer.ibu > ibuLow
-        })
-    }
-    if (ibu_lt) {
-        const ibuHigh = parseInt(ibu_lt)
-        filteredBeerData = filteredBeerData.filter(beer => {
-            return beer.ibu < ibuHigh
-        })
-    }
-    console.log('filtered beer data',filteredBeerData)
-    return filteredBeerData
-}
+const filterByIbuGreaterThan = (beerData: BeerType[], query: string) => {
+  return beerData.filter((beer) => beer.ibu > parseFloat(query));
+};
+
+const filterByIbuLessThan = (beerData: BeerType[], query: string) => {
+  return beerData.filter((beer) => beer.ibu < parseFloat(query));
+};
+
+const queryFilter = (beerData: BeerType[], query: Query) => {
+  const { beer_name, abv_gt, abv_lt, ibu_gt, ibu_lt } = query;
+  if (beer_name) beerData = filterByName(beerData, beer_name);
+  if (abv_gt) beerData = filterByAbvGreaterThan(beerData, abv_gt);
+  if (abv_lt) beerData = filterByAbvLessThan(beerData, abv_lt);
+  if (ibu_gt) beerData = filterByIbuGreaterThan(beerData, ibu_gt);
+  if (ibu_lt) beerData = filterByIbuLessThan(beerData, ibu_lt);
+
+  return beerData;
+};
 
 export default queryFilter;
