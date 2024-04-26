ec6e4f7aaa4e94f185cca8b6cd3b1e84b5bd0d6e
Scoopsies <mooninites103@gmail.com>
2024-04-25 22:33:56 -0500

    added a testing console log

--- routes/Beer.ts
+++ routes/Beer.ts
@@ -3,8 +3,8 @@ const beerRoute = express.Router()
 import BeerType from "../types/Beertype.types";
 const punkapi: BeerType[] = require('punkapi-db').sort((a : BeerType, b : BeerType) => a.id - b.id);
 
-
 beerRoute.get('/:id', (req, res) => {
+    console.log('Individual Beer Accessed')
     const beerId = parseInt(req.params.id)
     res.json(punkapi.find(beer => beer.id === beerId))
 })
